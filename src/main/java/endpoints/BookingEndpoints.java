13f830084c0ed7e114a3c463efea95b78bb483b4
hrithik <hrneogi99@gmail.com>
2024-05-01 17:54:57 +0530

    Initial setup added

---
+++ src/main/java/endpoints/BookingEndpoints.java
@@ -0,0 +1,6 @@
+package endpoints;
+
+public class BookingEndpoints {
+    public static final String BOOKINGS = "/booking";
+    public static final String BOOKINGS_AUTH = "/auth";
+}
---
+++ src/main/java/utils/ResponseValidator.java
@@ -0,0 +1,9 @@
+package utils;
+
+import io.restassured.response.Response;
+
+public class ResponseValidator {
+    public static void validateStatusCode(Response response, int expectedStatusCode) {
+        response.then().statusCode(expectedStatusCode);
+    }
+}
